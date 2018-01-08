664c77294e136acf45210868877345dadc863093
hdpuk86 <hayley.d.prior@gmail.com>
2018-01-08 22:03:50 +0000

    adds image

--- src/components/Film.js
+++ src/components/Film.js
@@ -4,7 +4,10 @@ class Film extends React.Component {
   render(){
     return(
       <div className="film">
-        <p>{this.props.title}</p>
+        <div className="film-title">
+          <img src="./bookmark.png" alt="bookmark" height="40px"/>
+          <p>{this.props.title}</p>
+        </div>
         <p>Showtimes</p>
       </div>
     )
